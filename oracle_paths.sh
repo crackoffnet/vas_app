4049dbf3c858705cdd8fd5bf3c8e9694cc861123
crackoffnet <crackoffnet@gmail.com>
2017-10-16 14:11:25 +0400

    Update oracle_paths.sh

--- oracle_paths.sh
+++ oracle_paths.sh
@@ -4,4 +4,4 @@
 echo "/usr/lib/oracle/11.1/client64/lib/" >> /etc/ld.so.conf.d/oracle.conf
 export ORACLE_HOME=/usr/lib/oracle/11.1/client64
 export PATH=$PATH:$ORACLE_HOME/bin
-export LD_LIBRARY_PATH=/usr/lib/oracle/11.1/client64/lib/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH=/usr/lib/oracle/11.1/client64/lib/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
