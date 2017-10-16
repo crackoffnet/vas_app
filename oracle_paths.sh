8201d4017c02f3bc1e52772acc0ccdc28837ebf8
crackoffnet <crackoffnet@gmail.com>
2017-10-16 12:32:35 +0400

    Update oracle_paths.sh

--- oracle_paths.sh
+++ oracle_paths.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
+###Integrate Oracle Libraries and export ORACLE_HOME
 echo "/usr/lib/oracle/11.1/client64/lib/" >> /etc/ld.so.conf.d/oracle.conf
-echo "export ORACLE_HOME=/usr/lib/oracle/11.1/client64
+export ORACLE_HOME=/usr/lib/oracle/11.1/client64
 export PATH=$PATH:$ORACLE_HOME/bin
-export
-LD_LIBRARY_PATH=/usr/lib/oracle/11.1/client64/lib/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" >> /etc/profile.d/oracle.sh
+export LD_LIBRARY_PATH=/usr/lib/oracle/11.1/client64/lib/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
