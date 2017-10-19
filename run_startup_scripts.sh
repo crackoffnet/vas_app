520a6ec004e4d2066f28786c875577edf4c8f6c6
crackoffnet <crackoffnet@gmail.com>
2017-10-19 14:34:42 +0400

    Update and rename oracle_paths.sh to run_startup_scripts.sh

---
+++ run_startup_scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+nohup java -jar /var/www/html/backend_jars/vta-oar.jar $2 > /var/log/vta-oar.log & sleep 5
+nohup java -jar /var/www/html/backend_jars/vta-vas.jar $2> /var/log/vta-vas.log & sleep 5
+$CATALINA_HOME/bin/startup.sh &
+service apache2 reload
+/usr/sbin/apache2ctl start
