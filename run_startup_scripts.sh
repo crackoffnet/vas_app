4794c482818b46d2ee0ad934bd49c5d6b6d01009
crackoffnet <crackoffnet@gmail.com>
2017-10-19 15:45:13 +0400

    Update run_startup_scripts.sh

--- run_startup_scripts.sh
+++ run_startup_scripts.sh
@@ -3,5 +3,5 @@
 nohup java -jar /var/www/html/backend_jars/vta-oar.jar $2 > /var/log/vta-oar.log & sleep 5
 nohup java -jar /var/www/html/backend_jars/vta-vas.jar $2> /var/log/vta-vas.log & sleep 5
 #$CATALINA_HOME/bin/startup.sh & sleep 5
-service apache2 reload
-/usr/sbin/apache2ctl start
+#service apache2 reload
+#/usr/sbin/apache2ctl start
