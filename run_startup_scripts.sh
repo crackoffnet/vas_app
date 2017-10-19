0df63e812fd5eb2c114308a799dbc07d37878a51
Garegin Ayvazyan <crackoffnet@gmail.com>
2017-10-20 00:12:22 +0400

    Fine

--- run_startup_scripts.sh
+++ run_startup_scripts.sh
@@ -2,8 +2,9 @@
 
 exec nohup java -jar /var/www/html/backend_jars/vta-oar.jar > /var/log/vta-oar.log &
 exec nohup java -jar /var/www/html/backend_jars/vta-vas.jar > /var/log/vta-vas.log &
+/bin/sh /usr/local/tomcat/bin/startup.sh
 a2enconf servername
 service apache2 reload
 exec /usr/sbin/apache2ctl -D FOREGROUND
-$CATALINA_HOME/bin/startup.sh
+
 
